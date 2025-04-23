8e2eff0a3c3767dbf9c51d148b399a565b7d7bad
Bharti Pund <bharti.pund@devotrend.com>
2025-04-23 17:33:10 +0530

    Add backup: full_mysql_backup_2025-04-23T12-02-37-759Z

--- full_mysql_backup/sql/backup_2025-04-23T12-02-37-759Z.sql
+++ full_mysql_backup/sql/backup_2025-04-23T12-02-37-759Z.sql
@@ -46,7 +46,7 @@ CREATE TABLE `databasechangelog` (
 
 LOCK TABLES `databasechangelog` WRITE;
 /*!40000 ALTER TABLE `databasechangelog` DISABLE KEYS */;
-INSERT INTO `databasechangelog` VALUES ('table-example_table2-0','auto','changelog.xml','2025-04-22 19:25:59',1,'EXECUTED','9:e7a388f597182344e25560367b467762','createTable tableName=example_table2','',NULL,'4.31.0',NULL,NULL,'5330156236'),('table-flywayusers11-0','auto','changelog.xml','2025-04-22 20:06:13',2,'EXECUTED','9:c30c4f3fda2c389f6814bca788f771ea','createTable tableName=flywayusers11','',NULL,'4.31.0',NULL,NULL,'5332568846');
+INSERT INTO `databasechangelog` VALUES ('table-example_table2-0','auto','changelog.xml','2025-04-22 19:25:59',1,'EXECUTED','9:e7a388f597182344e25560367b467762','createTable tableName=example_table2','',NULL,'4.31.0',NULL,NULL,'5330156236'),('table-flywayusers11-0','auto','changelog.xml','2025-04-22 20:06:13',2,'EXECUTED','9:c30c4f3fda2c389f6814bca788f771ea','createTable tableName=flywayusers11','',NULL,'4.31.0',NULL,NULL,'5332568846'),('table-table3-0','auto','changelog.xml','2025-04-23 17:19:24',3,'EXECUTED','9:31075efccc80a889fc9cf60bb2bfd15d','createTable tableName=table3','',NULL,'4.31.0',NULL,NULL,'5408961502');
 /*!40000 ALTER TABLE `databasechangelog` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,6 +121,31 @@ LOCK TABLES `flywayusers11` WRITE;
 /*!40000 ALTER TABLE `flywayusers11` DISABLE KEYS */;
 /*!40000 ALTER TABLE `flywayusers11` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `table3`
+--
+
+DROP TABLE IF EXISTS `table3`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `table3` (
+  `id` int DEFAULT NULL,
+  `Field1` varchar(500) DEFAULT NULL,
+  `Field2` varchar(500) DEFAULT NULL,
+  `Field3` varchar(500) DEFAULT NULL,
+  `Field4` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table3`
+--
+
+LOCK TABLES `table3` WRITE;
+/*!40000 ALTER TABLE `table3` DISABLE KEYS */;
+/*!40000 ALTER TABLE `table3` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -131,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-23 17:18:52
+-- Dump completed on 2025-04-23 17:32:37
