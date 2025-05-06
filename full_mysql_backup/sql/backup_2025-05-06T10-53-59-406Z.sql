7195e294ebfb16e7b00f9179bd89acf886d38932
Bharti Pund <bharti.pund@devotrend.com>
2025-05-06 16:24:11 +0530

    Add backup: full_mysql_backup_2025-05-06T10-53-59-406Z

--- full_mysql_backup/sql/backup_2025-05-06T10-53-59-406Z.sql
+++ full_mysql_backup/sql/backup_2025-05-06T10-53-59-406Z.sql
@@ -16,6 +16,78 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `connections`
+--
+
+DROP TABLE IF EXISTS `connections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `connections` (
+  `id` int DEFAULT NULL,
+  `connection_name` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `host` varchar(100) DEFAULT NULL,
+  `port` int DEFAULT NULL,
+  `database_name` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `url` text,
+  `password` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connections`
+--
+
+LOCK TABLES `connections` WRITE;
+/*!40000 ALTER TABLE `connections` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connections` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `custom_changelog_log`
+--
+
+DROP TABLE IF EXISTS `custom_changelog_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `custom_changelog_log` (
+  `id` int DEFAULT NULL,
+  `user_id` varchar(500) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `userType` varchar(255) DEFAULT NULL,
+  `company_name` varchar(255) DEFAULT NULL,
+  `company_id` varchar(255) DEFAULT NULL,
+  `changelog_file` varchar(500) DEFAULT NULL,
+  `changelog_label` varchar(255) DEFAULT NULL,
+  `git_repo` varchar(500) DEFAULT NULL,
+  `commit_message` text,
+  `git_commit_id` varchar(100) DEFAULT NULL,
+  `migration_status` varchar(50) DEFAULT NULL,
+  `git_status` varchar(50) DEFAULT NULL,
+  `migration_output` text,
+  `git_output` text,
+  `target_db_url` varchar(500) DEFAULT NULL,
+  `target_Connection_Name` varchar(500) DEFAULT NULL,
+  `source_db_url` varchar(500) DEFAULT NULL,
+  `source_Connection_Name` varchar(500) DEFAULT NULL,
+  `migration_Dtails` varchar(500) DEFAULT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `itemAdded` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `custom_changelog_log`
+--
+
+LOCK TABLES `custom_changelog_log` WRITE;
+/*!40000 ALTER TABLE `custom_changelog_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `custom_changelog_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `database_connections`
 --
 
@@ -74,7 +146,7 @@ CREATE TABLE `databasechangelog` (
 
 LOCK TABLES `databasechangelog` WRITE;
 /*!40000 ALTER TABLE `databasechangelog` DISABLE KEYS */;
-INSERT INTO `databasechangelog` VALUES ('table-table2-0','auto','changelog.xml','2025-04-25 19:57:57',1,'EXECUTED','9:73ba4c26ab690a0828fae7af0989ab08','createTable tableName=table2','',NULL,'4.31.0',NULL,NULL,'5591274245'),('table-flywayusers11-0','auto','changelog.xml','2025-04-25 20:01:49',2,'EXECUTED','9:c30c4f3fda2c389f6814bca788f771ea','createTable tableName=flywayusers11','',NULL,'4.31.0',NULL,NULL,'5591505443'),('table-example_table2-0','auto','changelog.xml','2025-05-05 16:27:51',3,'EXECUTED','9:e7a388f597182344e25560367b467762','createTable tableName=example_table2','',NULL,'4.31.0',NULL,NULL,'6442667876'),('table-git_connections-1','auto','changelog.xml','2025-05-05 16:27:51',4,'EXECUTED','9:1e5b47222c3f55b8037516c8d5964482','createTable tableName=git_connections','',NULL,'4.31.0',NULL,NULL,'6442667876'),('table-table3-0','auto','changelog.xml','2025-05-06 11:56:38',5,'EXECUTED','9:31075efccc80a889fc9cf60bb2bfd15d','createTable tableName=table3','',NULL,'4.31.0',NULL,NULL,'6512795186'),('table-tbl_register-0','auto','changelog.xml','2025-05-06 12:02:48',6,'EXECUTED','9:284002c8724e721465a4c338fe5fb277','createTable tableName=tbl_register','',NULL,'4.31.0',NULL,NULL,'6513164698'),('view-view_all_names-0','auto','changelog.xml','2025-05-06 12:17:34',7,'EXECUTED','9:4433419a6d9a4fd9fc499a82f1cb22f6','createView viewName=view_all_names','',NULL,'4.31.0',NULL,NULL,'6514051085'),('table-flyway_schema_history-0','auto','changelog.xml','2025-05-06 12:22:40',8,'EXECUTED','9:5fc63a116e7534b60646a1694c327e13','createTable tableName=flyway_schema_history','',NULL,'4.31.0',NULL,NULL,'6514357095'),('table-flyway_schema_old-0','auto','changelog.xml','2025-05-06 12:26:20',9,'EXECUTED','9:fd8fc44457b3edf9249462a99457095f','createTable tableName=flyway_schema_old','',NULL,'4.31.0',NULL,NULL,'6514577157'),('table-database_connections-0','auto','changelog.xml','2025-05-06 14:38:49',10,'EXECUTED','9:1b6ab163925c05835c6ec11ac7c046a9','createTable tableName=database_connections','',NULL,'4.31.0',NULL,NULL,'6522526333'),('table-tbl_user-0','auto','changelog.xml','2025-05-06 14:40:59',11,'EXECUTED','9:ac8fa139359aaf1549a65aa854803819','createTable tableName=tbl_user','',NULL,'4.31.0',NULL,NULL,'6522656365');
+INSERT INTO `databasechangelog` VALUES ('table-table2-0','auto','changelog.xml','2025-04-25 19:57:57',1,'EXECUTED','9:73ba4c26ab690a0828fae7af0989ab08','createTable tableName=table2','',NULL,'4.31.0',NULL,NULL,'5591274245'),('table-flywayusers11-0','auto','changelog.xml','2025-04-25 20:01:49',2,'EXECUTED','9:c30c4f3fda2c389f6814bca788f771ea','createTable tableName=flywayusers11','',NULL,'4.31.0',NULL,NULL,'5591505443'),('table-example_table2-0','auto','changelog.xml','2025-05-05 16:27:51',3,'EXECUTED','9:e7a388f597182344e25560367b467762','createTable tableName=example_table2','',NULL,'4.31.0',NULL,NULL,'6442667876'),('table-git_connections-1','auto','changelog.xml','2025-05-05 16:27:51',4,'EXECUTED','9:1e5b47222c3f55b8037516c8d5964482','createTable tableName=git_connections','',NULL,'4.31.0',NULL,NULL,'6442667876'),('table-table3-0','auto','changelog.xml','2025-05-06 11:56:38',5,'EXECUTED','9:31075efccc80a889fc9cf60bb2bfd15d','createTable tableName=table3','',NULL,'4.31.0',NULL,NULL,'6512795186'),('table-tbl_register-0','auto','changelog.xml','2025-05-06 12:02:48',6,'EXECUTED','9:284002c8724e721465a4c338fe5fb277','createTable tableName=tbl_register','',NULL,'4.31.0',NULL,NULL,'6513164698'),('view-view_all_names-0','auto','changelog.xml','2025-05-06 12:17:34',7,'EXECUTED','9:4433419a6d9a4fd9fc499a82f1cb22f6','createView viewName=view_all_names','',NULL,'4.31.0',NULL,NULL,'6514051085'),('table-flyway_schema_history-0','auto','changelog.xml','2025-05-06 12:22:40',8,'EXECUTED','9:5fc63a116e7534b60646a1694c327e13','createTable tableName=flyway_schema_history','',NULL,'4.31.0',NULL,NULL,'6514357095'),('table-flyway_schema_old-0','auto','changelog.xml','2025-05-06 12:26:20',9,'EXECUTED','9:fd8fc44457b3edf9249462a99457095f','createTable tableName=flyway_schema_old','',NULL,'4.31.0',NULL,NULL,'6514577157'),('table-database_connections-0','auto','changelog.xml','2025-05-06 14:38:49',10,'EXECUTED','9:1b6ab163925c05835c6ec11ac7c046a9','createTable tableName=database_connections','',NULL,'4.31.0',NULL,NULL,'6522526333'),('table-tbl_user-0','auto','changelog.xml','2025-05-06 14:40:59',11,'EXECUTED','9:ac8fa139359aaf1549a65aa854803819','createTable tableName=tbl_user','',NULL,'4.31.0',NULL,NULL,'6522656365'),('table-connections-0','auto','changelog.xml','2025-05-06 15:07:12',12,'EXECUTED','9:7ea73d14ea6ec506d74b7069aa0dea20','createTable tableName=connections','',NULL,'4.31.0',NULL,NULL,'6524228979'),('table-custom_changelog_log-0','auto','changelog.xml','2025-05-06 16:19:40',13,'EXECUTED','9:7862579f909605f456b2e7b04dc96718','createTable tableName=custom_changelog_log','',NULL,'4.31.0',NULL,NULL,'6528575467');
 /*!40000 ALTER TABLE `databasechangelog` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -409,4 +481,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-06 15:05:53
+-- Dump completed on 2025-05-06 16:23:59
